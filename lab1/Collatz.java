8d26b062e662745ca9e97804da0ff762bf12acd7
asdfghjkoiuy <huhhdh@outlook.com>
2025-09-17 18:14:05 +0800

    collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,6 +4,7 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
+    /** 
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
@@ -13,10 +14,21 @@ public class Collatz {
             return n * 2;
         }
     }
+    */
 
+    public static int nextNumber(int n){
+        if(n==1){
+            return 0;
+        }
+        else if((n&1)==1){
+            return 3*n+1;
+        }else{
+            return n/2;
+        }
+    }
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");  //try to push
+        System.out.print(n + " ");  
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
